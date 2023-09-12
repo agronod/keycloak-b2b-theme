2e0a497fa1c722e08dccd60d6a1c4e819f67e736
ZvjezdanavisosevicAgronod <129842791+ZvjezdanavisosevicAgronod@users.noreply.github.com>
2023-09-12 08:23:35 +0200

    refactoring and adding reset password page (#3)
    Feat
    refactoring and adding reset password page
    
    ---------
    
    Co-authored-by: Zvjezdana Visosevic <zvjezdanavisosevic@Zvjezdanas-MacBook-Pro.local>
    Co-authored-by: Zvjezdana Visosevic <zvjezdanavisosevic@Zvjezdanas-MBP.lan>

--- src/KcApp/i18n.ts
+++ src/KcApp/i18n.ts
@@ -17,11 +17,17 @@ export function useI18n(props: Props) {
         // that is "Forgot Password?" see: https://github.com/InseeFrLab/keycloakify/blob/f0ae5ea908e0aa42391af323b6d5e2fd371af851/src/lib/i18n/generated_messages/18.0.1/login/en.ts#L17
         doForgotPassword: "I forgot my password",
         doLogIn: "Next",
+        continue: "Continue",
         mfaValidationTitle: "Verify your email address",
         mfaValidationSubtitle:
           "A verification code has been sent to your email address. If you don't find it in your inbox, it may have ended up in your spam folder.",
         backToLogin: "Back to login",
         loginTitle: "Log in",
+        choosePassword: "Choose password",
+        newPassword: "New password",
+        confirmPassword: "Confirm password",
+        choosePasswordInfo:
+          "You must choose your own password to activate your account.",
         mfaVerificationInfoText: "Haven't received a verification code?",
         mfaVerficationCodeLink: "Send a new code",
       },
@@ -34,11 +40,17 @@ export function useI18n(props: Props) {
         gender: "Genre",
         doForgotPassword: "J'ai oublié mon mot de passe",
         doLogIn: "Suivant",
+        continue: "Continuer",
         mfaValidationTitle: "Vérifiez votre adresse e-mail",
         mfaValidationSubtitle:
           "Un code de vérification a été envoyé à votre adresse e-mail. Si vous ne le trouvez pas dans votre boîte de réception, il peut se trouver dans vos courriers indésirables.",
         backToLogin: "Retour à la connexion",
         loginTitle: "Connexion",
+        choosePassword: "Choisissez un mot de passe",
+        newPassword: "Nouveau mot de passe",
+        confirmPassword: "Répéter le mot de passe",
+        choosePasswordInfo:
+          "Vous devez choisir votre propre mot de passe pour activer votre compte.",
         mfaVerificationInfoText: "Haven't received a verification code?",
         mfaVerficationCodeLink: "Send a new code",
         /* spell-checker: enable */
@@ -51,11 +63,17 @@ export function useI18n(props: Props) {
         gender: "Kön",
         doForgotPassword: "Jag har glömt mitt lösenord",
         doLogIn: "Nästa",
+        continue: "Fortsätt",
         mfaValidationTitle: "Verifiera din e-postadress",
         mfaValidationSubtitle:
           "En verifieringskod har skickats till din e-postadress. Hittar du den inte i inkorgen kan den ha hamnat som skräppost.",
         backToLogin: "Tillbaka till inloggning",
         loginTitle: "Logga in",
+        choosePassword: "Välj lösenord",
+        newPassword: "Nytt lösenord",
+        confirmPassword: "Upprepa lösenord",
+        choosePasswordInfo:
+          "Du måste välja ett eget lösenord för att aktivera ditt konto.",
         mfaVerificationInfoText: "Har du inte fått en verifieringskod?",
         mfaVerficationCodeLink: "Skicka en ny kod",
       },
