d401fb93f4ada5468c533be35a06bad565e6c832
robinsmedberg <robin.smedberg@gmail.com>
2025-09-24 10:28:51 +0200

    feat: implement v11 (#9)
    * implement login page from updated keycloak-starter
    
    ignore .local
    
    remove .local
    
    * switch to yarn
    
    * use actions/upload-artifact@v3
    
    * upload-artifact@v4
    
    * use secrets.GITHUBTOKEN
    
    * Revert "use secrets.GITHUBTOKEN"
    
    This reverts commit 9b34ae8022335f0c6205008482a50853bf1d9b42.
    
    * try useing .npmrc file
    
    * actions/download-artifact@v4
    
    * all files
    
    * use github_token
    
    * set prerelease
    
    * add reset password
    
    * add mfa-validation
    
    * rename
    
    * fix repo
    
    * dry run when pull request
    
    * fix large background image
    
    * remove minimatch
    
    * remove viteImagemin
    
    * ai-docs
    
    * cleaning
    
    * translations
    
    * only 1 release artifact
    
    * fix translations

--- src/components/AsideLogoSection/AsideLogoSection.tsx
+++ src/components/AsideLogoSection/AsideLogoSection.tsx
@@ -1,94 +1,158 @@
 import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
-import Logo from "components/Logo";
-import AgronodBackgroundDesktop from "../../assets/agronodBackgroundDesktop.png";
-import AgronodYellowBackground from "../../assets/agronodYellowBackground.png";
+import Logo from "../Logo";
+import Picture from "../Picture";
+import agronodBackgroundDesktopWebP from "../../assets/agronodBackgroundDesktop.webp";
+import agronodBackgroundDesktopPNG from "../../assets/agronodBackgroundDesktop.png";
+import agronodYellowBackgroundWebP from "../../assets/agronodYellowBackground.webp";
+import agronodYellowBackgroundPNG from "../../assets/agronodYellowBackground.png";
 
 const AsideLogo = () => {
-  const theme = useTheme();
-  const isSmallLaptop = useMediaQuery(theme.breakpoints.down("lg"));
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  return (
-    <>
-      {!isMobile ? (
-        <Box
-          sx={() => ({
-            display: "flex",
-            flexDirection: "column",
-            position: "relative",
-            backgroundColor: theme.palette.primary.main,
-          })}
-        >
-          <Box
-            sx={(theme) => ({
-              backgroundColor: theme.palette.primary.main,
-              padding: 6,
-              height: "40%",
-
-              [theme.breakpoints.down("lg")]: {
-                height: "70%",
-              },
-            })}
-          >
-            <Logo size="small" />
-            <Box
-              sx={(theme) => ({
-                maxWidth: "450px",
-                marginTop: 3,
-                [theme.breakpoints.down("lg")]: {
-                  maxWidth: "350px",
-                },
-              })}
-            >
-              <Typography variant={isSmallLaptop ? "h4" : "h2"}>
-                Vi frigör kraften i lantbrukets data
-              </Typography>
-            </Box>
-          </Box>
-          <Box
-            sx={(theme) => ({
-              height: "60%",
-              width: "100%",
-              background: `url(${AgronodBackgroundDesktop}) center center no-repeat`,
-              backgroundSize: "cover",
-              [theme.breakpoints.down("lg")]: {
-                height: "30%",
-              },
-            })}
-          >
-            <Box
-              sx={{
-                background: `url(${AgronodYellowBackground}) center center repeat`,
-                backgroundSize: "contain",
-                height: "100%",
-                width: "100%",
-              }}
-            ></Box>
-          </Box>
-        </Box>
-      ) : (
-        <Box
-          sx={(theme) => ({
-            backgroundColor: theme.palette.primary.main,
-            display: "flex",
-            flexDirection: "column",
-            background: `url(${AgronodYellowBackground}) center center repeat , url(${AgronodBackgroundDesktop}) center center no-repeat`,
-            backgroundSize: "cover, 100% 99%",
-            width: "100%",
-            maxWidth: "100%",
-            textAlign: "left",
-            marginBottom: 5,
-            position: "fixed",
-            top: 0,
-            backgroundPosition: "center",
-            height: "219px",
-            padding: 3,
-          })}
-        >
-          <Logo size="small" />
-        </Box>
-      )}
-    </>
-  );
+    const theme = useTheme();
+    const isSmallLaptop = useMediaQuery(theme.breakpoints.down("lg"));
+    const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+    return (
+        <>
+            {!isMobile ? (
+                <Box
+                    sx={() => ({
+                        position: "relative",
+                        backgroundColor: theme.palette.primary.main,
+                        height: "100%",
+                        width: "100%",
+                        overflow: "hidden"
+                    })}
+                >
+                    {/* Background landscape image */}
+                    <Box
+                        sx={theme => ({
+                            position: "absolute",
+                            width: "100%",
+                            height: "60%",
+                            left: "0",
+                            bottom: "0",
+                            overflow: "hidden",
+                            backgroundColor: theme.palette.primary.main,
+                            zIndex: 1
+                        })}
+                    >
+                        <Picture
+                            webpSrc={agronodBackgroundDesktopWebP}
+                            fallbackSrc={agronodBackgroundDesktopPNG}
+                            alt="Background"
+                            style={{
+                                position: "absolute",
+                                width: "100%",
+                                height: "100%",
+                                objectFit: "cover"
+                            }}
+                        />
+                    </Box>
+                    
+                    {/* Yellow overlay with circular cutouts */}
+                    <Box
+                        sx={{
+                            position: "absolute",
+                            width: "100%",
+                            height: "60%",
+                            left: "0",
+                            bottom: "0",
+                            overflow: "hidden",
+                            zIndex: 2
+                        }}
+                    >
+                        <Picture
+                            webpSrc={agronodYellowBackgroundWebP}
+                            fallbackSrc={agronodYellowBackgroundPNG}
+                            alt=""
+                            style={{
+                                position: "absolute",
+                                width: "100%",
+                                height: "100%",
+                                objectFit: "cover",
+                                objectPosition: "center top",
+                            }}
+                        />
+                    </Box>
+                    
+                    {/* Logo and text content */}
+                    <Box
+                        sx={theme => ({
+                            position: "relative",
+                            zIndex: 3,
+                            padding: 6,
+                            [theme.breakpoints.down("lg")]: {
+                                padding: 4
+                            }
+                        })}
+                    >
+                        <Logo size="small" />
+                        <Box
+                            sx={theme => ({
+                                maxWidth: "450px",
+                                marginTop: 3,
+                                [theme.breakpoints.down("lg")]: {
+                                    maxWidth: "350px"
+                                }
+                            })}
+                        >
+                            <Typography variant={isSmallLaptop ? "h4" : "h2"}>
+                                Vi frigör kraften i lantbrukets data
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Box>
+            ) : (
+                <Box
+                    sx={theme => ({
+                        backgroundColor: theme.palette.primary.main,
+                        display: "flex",
+                        flexDirection: "column",
+                        width: "100%",
+                        maxWidth: "100%",
+                        textAlign: "left",
+                        marginBottom: 5,
+                        position: "fixed",
+                        top: 0,
+                        height: "219px",
+                        padding: 3,
+                        overflow: "hidden"
+                    })}
+                >
+                    <Picture
+                        webpSrc={agronodBackgroundDesktopWebP}
+                        fallbackSrc={agronodBackgroundDesktopPNG}
+                        alt="Background"
+                        style={{
+                            position: "absolute",
+                            top: 0,
+                            left: 0,
+                            width: "100%",
+                            height: "99%",
+                            objectFit: "cover",
+                            zIndex: 1
+                        }}
+                    />
+                    <Picture
+                        webpSrc={agronodYellowBackgroundWebP}
+                        fallbackSrc={agronodYellowBackgroundPNG}
+                        alt=""
+                        style={{
+                            position: "absolute",
+                            top: 0,
+                            left: 0,
+                            width: "100%",
+                            height: "100%",
+                            objectFit: "cover",
+                            zIndex: 2
+                        }}
+                    />
+                    <Box sx={{ position: "relative", zIndex: 3 }}>
+                        <Logo size="small" />
+                    </Box>
+                </Box>
+            )}
+        </>
+    );
 };
 
 export default AsideLogo;
